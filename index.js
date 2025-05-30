d5d986f9b77ef0edcf6a5535b64234937bd7e9bb
GLOBOCHAIN <jehanno1972@gmail.com>
2025-05-30 19:06:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>GLOBOCHAIN</h1>
+      <p>Une solution technologique pour l’accès sécurisé aux données de santé des voyageurs.</p>
+      <p><a href="/faq">Consultez la FAQ</a> | <a href="/pro">Espace professionnel</a></p>
+    </div>
+  );
+}
