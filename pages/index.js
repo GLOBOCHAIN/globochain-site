87bae548c64b8cdea7968eb274aa02fc23f15cd0
GLOBOCHAIN <jehanno1972@gmail.com>
2025-05-31 19:20:27 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Bienvenue sur <strong>GLOBOCHAIN</strong></h1>
-      <p>Le prototype est bien en place. Cette page simule le site MVP.</p>
-    </div>
-  );
-}
